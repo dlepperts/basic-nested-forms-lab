18de8e1764a51e90070a5eb67f9b31aba87125b1
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-01-20 09:52:29 -0600

    changes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,15 @@ class RecipesController < ApplicationController
   end
 
   def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipes_path
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title)
+  end
+
 end
+
