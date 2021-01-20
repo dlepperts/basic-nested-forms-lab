fe97579a71f709e00fa40490c0c9c7acf55e04cb
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-01-20 10:14:36 -0600

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  # accepts_nested_attributes_for line allows a recipe instance to take in a nested hash as attributes
   accepts_nested_attributes_for :ingredients
 end
